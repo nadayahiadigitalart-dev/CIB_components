a3fa8ca895d5965973e90d51e60e583b739dbc04
Nadayahia_dev <nadayahiadigitalart@gmail.com>
2025-10-31 19:09:31 +0200

    created Button and finished Hero

---
+++ cib-components/src/common/Button.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+
+const Botton = () => {
+    return ( 
+
+
+        <div className='bu' >Discover more</div>
+
+
+     );
+}
+ 
+export default Botton;
--- cib-components/src/pages/Home.jsx
+++ cib-components/src/pages/Home.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Navbar from '../components/layout/ Navbar';
 import './Home.css';
 import Logo from '../assets/logomain.png';
+import Button from '../common/Button';
+
+
+
+
+
 
 const Home = () => {
     return ( 
@@ -22,7 +28,20 @@ const Home = () => {
 
         <section className='hero'>
 
+            <div className='col'>
+                <h1 className='fb'>CIB</h1>
+                <p className='f'>Delivering value to our clients, our <br></br>
+                community and our shareholders</p>
 
+                <Button />
+
+            </div>
+
+        </section>
+
+
+        <section className='words'>
+            
 
         </section>
 
